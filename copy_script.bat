d54a67707b7ab3c3ec299b012e6e962008886b93
Valeriu Ionut Iocsa <valeriuionutiocsa@gmail.com>
2023-03-08 01:48:20 +0200

    updated scripts

--- copy_script.bat
+++ copy_script.bat
@@ -1,3 +1,3 @@
 @echo off
 
-copy /y %~dp0\copy_escaped_path.py C:\IVI\Apps\Scripts\General\copy_escaped_path.py
+py -3 -m PyInstaller -F -n CopyEscapedPath --distpath C:\IVI\Apps\Scripts\General\CopyEscapedPath .\copy_escaped_path.py
